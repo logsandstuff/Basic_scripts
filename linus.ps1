73a2c2b2504b8dc1175a8c091f5f991fa928af10
logsandstuff <59518250+logsandstuff@users.noreply.github.com>
2020-01-31 23:45:34 -0800

    Create linus.ps1

---
+++ linus.ps1
@@ -0,0 +1,19 @@
+(New-Object Net.WebClient).DownloadFile('https://www.cryptoprank.com/assets/images/petya_windows.png',"$env:Temp\b.jpg");
+
+$setwallpapersrc = @"
+using System.Runtime.InteropServices;
+public class wallpaper
+{
+public const int SetDesktopWallpaper = 20;
+public const int UpdateIniFile = 0x01;
+public const int SendWinIniChange = 0x02;
+[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+public static void SetWallpaper ( string path )
+{
+SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+}
+}
+"@
+Add-Type -TypeDefinition $setwallpapersrc
+[wallpaper]::SetWallpaper("$env:temp\b.jpg") 
